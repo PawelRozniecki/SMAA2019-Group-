149d948e810ee2c4106c557746eb427a90176f09
Pawel Rozniecki <pawelrozniecki13@outlook.com>
2019-11-13 16:52:38 +0100

    Uploaded first part of the code
    loading the data set

---
+++ classifier.py
@@ -0,0 +1,17 @@
+from sklearn.model_selection import train_test_split
+from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("data.csv", delimiter=',')
+#
+# x = data.iloc[: , :-1].values
+# y = data.iloc[:, -1].values
+#
+# X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_state = 0)
+
+
+
+print(data.head(len(data)))
